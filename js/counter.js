f34630aad7ea7e6e55651d39737563ef269ef4b5
Banhawy <the.benhawy@gmail.com>
2018-02-02 04:31:42 -0600

    Removed spaghetti code, tested and added List View

--- js/counter.js
+++ js/counter.js
@@ -1,52 +1,50 @@
-for (let i = 1; i < 8; i++) {
-    $(".sidebar").append(`<li id="cat${i}">Cat ${i}</li>`);
-    let elem = document.getElementById(`cat${i}`)
-    elem.addEventListener('click', (function(num) {
-        return function() {
-            let catShow = `<div class="counter${num}"><h2>Cat ${num}</h2><span id='counter${num}'>0</span></div>
-            <img id="cat-counter-${num}" src="img/cat${num}.jpg" alt="cute cat pic">`;
-            $('.cat-show').empty().append(catShow);
-            $(`#cat-counter-${num}`).on('click', (function(numCopy) {
-                return function() {
-                    var currentVal = $(`#counter${numCopy}`).text();
-                    var newVal = parseInt($(`#counter${numCopy}`).text()) + 1;
-                    $(`#counter${num}`).text(newVal);
-                }
-            })(num))
-        };
-    })(i))
-}
-
 $(function () {
     var model = {
         init: function () {
-            localStorage.cats = [];
+            this.cats = [];
             for (let i = 1; i < 8; i++) {
                 var cat = {
-                    name: `cat${i}`,
+                    name: `Cat${i}`,
                     counter: 0
                 }
-                localStorage.cats.push(cat);
+                this.cats.push(cat);
             }
         },
         getCat: function (num) {
-            return localStorage.cats[num];
+            return this.cats[num];
+        },
+        getAllCats: function () {
+            return this.cats;
         }
     };
 
     var octopus = {
-        getCats = function (num) {
+        getCat : function (num) {
             return model.getCat(num);
+        },
+        getAllCats: function () {
+            return model.getAllCats();
         },
         init: function () {
             model.init();
-            view.init();
+            listView.init();
         }
     };
 
-    var view = {
+    var listView = {
         init: function () {
-
+            this.catList = $('.cat-list');
+            this.cats = octopus.getAllCats();
+            listView.render();
+        },
+        render: function () {
+            var htmlStr = '';
+            this.cats.forEach(function (cat) {
+                htmlStr += `<li>${cat.name}</li>`;
+            });
+            this.catList.html(htmlStr);
         }
     };
+
+    octopus.init();
 })
