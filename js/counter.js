2becde3976c60a885d500b7fe7b19c5f909cf28e
Banhawy <the.benhawy@gmail.com>
2018-02-02 22:27:50 -0600

    Added admin view and toggle event

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,6 @@
-$(function () {
+$(function() {
     var model = {
-        init: function () {
+        init: function() {
             this.cats = [];
             for (let i = 1; i < 8; i++) {
                 var cat = {
@@ -12,75 +12,76 @@ $(function () {
                 this.cats.push(cat);
             }
         },
-        getCat: function (num) {
-            return this.cats[num-1];
+        getCat: function(num) {
+            return this.cats[num - 1];
         },
-        getAllCats: function () {
+        getAllCats: function() {
             return this.cats;
         },
-        getCurrentCat: function () {
+        getCurrentCat: function() {
             for (var i = 0; i < this.cats.length; i++) {
                 let cat = this.cats[i];
                 if (cat.currentCat) return cat;
 
             }
         },
-        setCurrenCat: function (num) {
+        setCurrenCat: function(num) {
             let current = this.getCurrentCat(num - 1);
             if (current) current.currentCat = false;
             this.cats[num - 1]['currentCat'] = true;
         },
-        increment: function (num) {
+        increment: function(num) {
             let cat = this.getCat(num);
             cat.counter++;
         }
     };
 
     var octopus = {
-        getCat : function (num) {
+        getCat: function(num) {
             return model.getCat(num);
         },
-        getAllCats: function () {
+        getAllCats: function() {
             return model.getAllCats();
         },
-        getCurrentCat: function () {
+        getCurrentCat: function() {
             return model.getCurrentCat();
         },
-        incrementCounter: function (num) {
+        incrementCounter: function(num) {
             return model.increment(num);
         },
-        setCatView: function () {
+        setCatView: function() {
             catView.init();
             catView.hide();
             catView.render();
         },
-        setCurrentCat: function (num) {
+        setCurrentCat: function(num) {
             return model.setCurrenCat(num);
         },
-        updateCounterView: function () {
+        updateCounterView: function() {
             catView.render();
         },
-        init: function () {
+        init: function() {
             model.init();
             listView.init();
+            admin.init();
         }
     };
 
     var listView = {
-        init: function () {
+        init: function() {
             this.catList = $('.cat-list');
             this.cats = octopus.getAllCats();
             listView.render();
         },
-        render: function () {
+        render: function() {
             var htmlStr = '';
-            this.cats.forEach(function (cat) {
+            this.cats.forEach(function(cat) {
                 htmlStr += `<li id="${cat.name}">${cat.name}</li>`;
             });
             this.catList.html(htmlStr);
-            this.cats.forEach(function (cat) {
-                $(`#${cat.name}`).on('click', (function (num) {
-                    return function () {
+            this.cats.forEach(function(cat) {
+                $(`#${cat.name}`).on('click', (function(num) {
+                    return function() {
                         octopus.setCurrentCat(num);
                         octopus.setCatView();
                     }
@@ -90,24 +91,36 @@ $(function () {
     };
 
     var catView = {
-        init: function () {
+        init: function() {
             this.catShow = $('.cat-show');
             this.currentCat = octopus.getCurrentCat();
-            catView.render();
         },
-        hide: function () {
+        hide: function() {
             this.catShow.empty();
         },
-        render: function () {
+        render: function() {
             let catDiv = `<div class="counter"><h2>${this.currentCat.name}</h2><span>${this.currentCat.counter}</span></div>
             <img id="cat-counter-${this.currentCat.catNumber}" src="img/cat${this.currentCat.catNumber}.jpg" alt="cute cat pic">`;
             this.catShow.html(catDiv);
-            $(`#cat-counter-${this.currentCat.catNumber}`).on('click', (function (num) {
-                return function () {
-                octopus.incrementCounter(num);
-                octopus.updateCounterView();
+            $(`#cat-counter-${this.currentCat.catNumber}`).on('click', (function(num) {
+                return function() {
+                    octopus.incrementCounter(num);
+                    octopus.updateCounterView();
                 }
-            })(this.currentCat.catNumber))
+            })(this.currentCat.catNumber));
+        }
+    }
+
+    var admin = {
+        init: function() {
+            this.adminButton = $('#admin');
+            this.adminButton.on('click', function() {
+                this.adminDiv = $('.admin');
+                this.adminDiv.toggle();
+            });
+        },
+        render: function() {
+
         }
     }
 
