2222ce39f771db3afca065b16c35674922309542
Banhawy <the.benhawy@gmail.com>
2018-02-02 19:10:16 -0600

    Fixed function closures and added new methods to model and controller and a separate view for cat images

--- js/counter.js
+++ js/counter.js
@@ -5,16 +5,38 @@ $(function () {
             for (let i = 1; i < 8; i++) {
                 var cat = {
                     name: `Cat${i}`,
-                    counter: 0
+                    counter: 0,
+                    catNumber: i,
+                    currentCat: false
                 }
                 this.cats.push(cat);
             }
         },
         getCat: function (num) {
-            return this.cats[num];
+            return this.cats[num-1];
         },
         getAllCats: function () {
             return this.cats;
+        },
+        getCurrentCat: function () {
+            for (var i = 0; i < this.cats.length; i++) {
+                let cat = this.cats[i];
+                console.log(cat);
+                if (cat.currentCat) return cat;
+
+            }
+            for (cat in this.cats) {
+            }
+        },
+        setCurrenCat: function (num) {
+            let current = this.getCurrentCat(num - 1);
+            if (current) current.currentCat = false;
+            console.log(this.cats[num - 1]['currentCat']);
+            this.cats[num - 1]['currentCat'] = true;
+            console.log(this.cats[num - 1]['currentCat']);
+        },
+        increment: function (num) {
+            this.getCat(num).counter++;
         }
     };
 
@@ -24,6 +46,20 @@ $(function () {
         },
         getAllCats: function () {
             return model.getAllCats();
+        },
+        getCurrentCat: function () {
+            return model.getCurrentCat();
+        },
+        incrementCounter: function (num) {
+            return model.increment(num);
+        },
+        setCatView: function () {
+            catView.init();
+            catView.hide();
+            catView.render();
+        },
+        setCurrentCat: function (num) {
+            return model.setCurrenCat(num);
         },
         init: function () {
             model.init();
@@ -40,11 +76,39 @@ $(function () {
         render: function () {
             var htmlStr = '';
             this.cats.forEach(function (cat) {
-                htmlStr += `<li>${cat.name}</li>`;
+                htmlStr += `<li id="${cat.name}">${cat.name}</li>`;
             });
             this.catList.html(htmlStr);
+            this.cats.forEach(function (cat) {
+                $(`#${cat.name}`).on('click', (function (num) {
+                    return function () {
+                        octopus.setCurrentCat(num);
+                        octopus.setCatView();
+                    }
+                })(cat.catNumber));
+            })
         }
     };
+
+    var catView = {
+        init: function () {
+            this.catShow = $('.cat-show');
+            this.currentCat = octopus.getCurrentCat();
+            console.log(this.currentCat);
+            catView.render();
+        },
+        hide: function () {
+            this.catShow.empty();
+        },
+        render: function () {
+            let catDiv = `<div class="counter"><h2>${this.currentCat.name}</h2><span>${this.currentCat.counter}</span></div>
+            <img id="cat-counter-${this.currentCat.catNumber}" src="img/cat${this.currentCat.catNumber}.jpg" alt="cute cat pic">`;
+            this.catShow.html(catDiv);
+            $(`#cat-counter-${this.currentCat.catNumber}`).on('click', (function (num) {
+                octopus.incrementCounter(num);
+            })(this.currentCat.catNumber))
+        }
+    }
 
     octopus.init();
 })
